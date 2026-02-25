9e701ccad677ca663d264af94c728fb8d1cb87e0
mosleym <mosleym@iu.edu>
2026-02-25 16:37:17 -0500

    saving

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # app.py -- the main file for the Streamlit app
 
 import streamlit as st
-from __init__ import *
 
 # What should we call the app?
 app_name = 'Rental Finder'
--- views/main_page.py
+++ views/main_page.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
+#--------------------------------Event Listeners-------------------------------
+def get_results():
+    # Get the search criteria
+    state = st.session_state['state-input']
+    bed_count = st.session_state['bedrooms-input']
+
+#----------------------------------Page Layout---------------------------------
 st.title('Rental Finder')
-st.write('put stuff here')
+
+# Gather input from the user
+st.text_input('Desired State', key='state-input')
+st.number_input('Desired Bedroom Count', key='bedrooms-input')
+st.button('Find my place!')
+st.divider()
+
+# Right now, we can just find the cheapest 
+
+# List the top results here
