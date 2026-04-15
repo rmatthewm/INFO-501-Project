ae807c06215f302187938aece4f0550d6dd75f80
Raul Matthew Mosley <raulmatthewmosley@gmail.com>
2026-04-15 13:28:00 -0400

    began recommendation model

--- data_classes/recommendation_model.py
+++ data_classes/recommendation_model.py
@@ -1,6 +1,7 @@
 # A model to recommend listings to the user
 from review_handler import ReviewHandler
 from data_handler import DataHandler
+from haversine import haversine, Unit
 
 class RecommendationModel:
     def __init__(self, review_handler, fmr_handler):
@@ -8,5 +9,19 @@ class RecommendationModel:
         self.rh = review_handler
         self.fmrh = fmr_handler
 
-    def recommend_listings(self, listings):
+    def score_listing(lat, long, distance, price):
+        return 
+
+    def recommend_listings(self, listings, lat, long, top=10):
+        # Calculate the distance from the desired coords
+        listings['distance'] = listings.apply(lambda row: haversine((lat, long), (row['latitude'], row['longitude']), Unit.MILES), axis=1)
+
+        # Calculate the scores for the listings
+        listings['score'] = listings.apply(lambda row: self.score_listing(row['latitude'], row['longitude'], row['distance'], row['price']), axis=1)
+
+        # Sort by the scores
+        listings.sort_values(by='score', ascending=False)
+
+        # Return the top items
+        return listings.iloc[:top]
 
